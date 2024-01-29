3fa9ec5ab22f0129d93eb57cae5eda684b49b2f1
Nantomiaro RALIBERA <94388805+nralibera@users.noreply.github.com>
2024-01-29 11:42:29 +0100

    Add color blending, wrap code in functions

--- app.js
+++ app.js
@@ -8,12 +8,10 @@ var svg = d3.select("#my_dataviz")
 
 // Map and projection
 const projection = d3.geoMercator()
-    .center([47.448,-21.397180])                // GPS of location to zoom on
-    .scale(1500)                       // This is like the zoom
+    .center([47.448,-21.397180])        
+    .scale(1500)                      
     .translate([ width/2, height/1.5 ])
 
-// let movable_svg = d3.select("#movable_svg_2")
-//                     .call(drag);
 
 const path = d3.geoPath().projection(projection);
 
@@ -23,50 +21,16 @@ svg.call(d3.zoom().on('zoom',()=>{
     }));
 
 
-// // Pattern Creation for Background
+function drawSecondCountries(mapName,countryPathData,centerCoordinateData){
+    let second_countries = topojson.feature(countryPathData, countryPathData.objects.countries);
+    second_countries.features = second_countries.features.filter(function(d){ return d.properties.name==mapName});
 
-// const pattern = svg.append('defs')
-// .append('pattern')
-//   .attr('id', 'greyDots')
-//   .attr('patternUnits', 'userSpaceOnUse')
-//   .attr('width', 4)
-//   .attr('height', 4);
-
-// pattern.append('circle')
-// .attr('cx', 2)
-// .attr('cy', 2)
-// .attr('r', 1);
-
-// const redPattern = svg.append('defs')
-//     .append('pattern')
-//       .attr('id', 'redDots')
-//       .attr('patternUnits', 'userSpaceOnUse')
-//       .attr('width', 4)
-//       .attr('height', 4);
-
-// redPattern.append('circle')
-// .attr('cx', 2)
-// .attr('cy', 2)
-// .attr('r', 1);
-
-
-function drawSecondCountries(mapName,countriesPath,countryData){
-    
-    // const firstCountry = countriesPath.features.filter(function(d){ return d.properties.name=="Madagascar"})
-    const country = countryData.filter(function(d){ return d.map_name==mapName})[0]
-    const features = countriesPath.features.filter(function(d){ return d.properties.name==mapName})
-    // const second_country_group = d3.select(".second_country_group");
-    // console.log(features)
-    // console.log(mapName)
-    
-
+    const averageCenter = centerCoordinateData.filter(function(d){ return d.map_name==mapName})[0];
     const projection2 = d3.geoMercator()
-    .center([country.long, country.lat])                 // GPS of location to zoom on
-    .scale(1500)                       // This is like the zoom
+    .center([averageCenter.long, averageCenter.lat])                
+    .scale(1500)
     .translate([ width/2, height/1.5 ])
-
     const path2 = d3.geoPath().projection(projection2);
-    // const pathFirstCountry = path(firstCountry[0]);
 
     // Define the drag behavior using D3's drag() function
     const drag = d3.drag()
@@ -92,13 +56,12 @@ function drawSecondCountries(mapName,countriesPath,countryData){
     .on('end', function () {
         // Remove the 'active' class when dragging ends
         d3.select(this).classed('active', false);
-        // drawSecondCountries(mapName,countriesPath,countryData)
+        // drawSecondCountries(mapName,countriesPath,centerCoordinateData)
     });
-
 
     d3.select(".all_countries")
     .selectAll(".second_country")
-        .data(features, function(d){return d.properties.name})
+        .data(second_countries.features, function(d){return d.properties.name})
         .join(
         enter => enter
         .append("path")
@@ -113,7 +76,68 @@ function drawSecondCountries(mapName,countriesPath,countryData){
           )
 }
 
+function howBig (countries,world,all_country_coordinate,regions){
 
+    // Draw the map
+    svg.append("g")
+        .attr("class", "all_countries")
+        .selectAll(".country")
+        .data(countries.features)
+        .join("path")
+        .attr("class", "country")
+          .attr("fill", "#00ccbc")
+          .attr("d", path)
+    
+    drawRegion(regions);
+
+    // Create a dropdown input
+    const dropdown = d3.select("body")
+    .append("select")
+    .attr("id", "countrySelect")
+    .on("change", function() {
+        const map_name = d3.select(this).property("value");
+        drawSecondCountries(map_name,world,all_country_coordinate);
+    });
+
+    // Add the country names as options
+    dropdown.selectAll("option")
+    .data(all_country_coordinate)
+    .enter()
+    .append("option")
+    .text(d => d.map_name)  
+    .attr("value", d => d.map_name);
+
+    // Add a random button to trigger the drawSecondCountries function
+    d3.select("body")
+    .append("button")
+    .attr("class", "randomButton")
+    .text("Random Country")
+    .on("click", function() {
+        const randomIndex = Math.floor(Math.random() * all_country_coordinate.length);
+        const map_name = all_country_coordinate[randomIndex].map_name;
+        drawSecondCountries(map_name,world,all_country_coordinate);
+        dropdown.property("value", map_name);
+    });
+}
+
+
+function drawRegion(regionData){
+    // How to load MDG REGIONS
+    svg.append("g")
+        .selectAll(".region") 
+        .data(regionData.features)
+        .join("path")
+        .attr("class", "region")
+        .attr("fill", "none")
+        .attr("d", path)
+        // .style("stroke", "green")
+        // .on("mouseover", function(event, d) {
+        //     // console.log(event)
+        //     d3.select(this).style("stroke", "blue")
+        //             .attr("fill", "blue")
+            
+        // })
+}
 
 
 // Load external data and boot
@@ -125,85 +149,26 @@ Promise.all([
     .then(function([world, adm_2,all_country_coordinate]){
 
         // Filter data
-        // data.features = data.features.filter(function(d){console.log(d.properties.name) ; return d.properties.name=="France"})
         let countries = topojson.feature(world, world.objects.countries);
-        let second_countries = topojson.feature(world, world.objects.countries);
-        let states = topojson.feature(adm_2, adm_2.objects.states);
+        let regions = topojson.feature(adm_2, adm_2.objects.states); // For Madagascar Regions
         
         
-        
+        // Draw Madagascar
         countries.features = countries.features.filter(function(d){ return d.properties.name=="Madagascar"})
+
         // Filter all_country_coordinate to only those with map_name
         all_country_coordinate = all_country_coordinate.filter(function(d){ return d.map_name!=""})
-        // Draw the map
-        svg.append("g")
-            .attr("class", "all_countries")
-            .selectAll(".country")
-            .data(countries.features)
-            .join("path")
-            .attr("class", "country")
-              .attr("fill", "#00ccbc")
-              .attr("d", path)
-              
 
+        // sort all_country_coordinate by map_name
+        all_country_coordinate.sort(function(a, b) {
+            return a.map_name.localeCompare(b.map_name);
+        });
 
-
-                    // Create a dropdown input
-            const dropdown = d3.select("body")
-            .append("select")
-            .attr("id", "countrySelect")
-            .style("position", "absolute")
-            .style("top", "10px")
-            .style("left", "10px")
-            .style("z-index", "1000")
-            .style('width', '200px')
-            .on("change", function() {
-                const map_name = d3.select(this).property("value");
-                // Remove the previous svg
-                // d3.select(".second_country").remove();
-                
-                drawSecondCountries(map_name,second_countries,all_country_coordinate);
-            });
-
-            // Add the country names as options
-            dropdown.selectAll("option")
-            .data(all_country_coordinate)
-            .enter()
-            .append("option")
-            .text(d => d.map_name)  // Display the country name
-            .attr("value", d => d.map_name);  // Use the ISO code as the value
+        // Clean duplicate map_name
+        all_country_coordinate = all_country_coordinate.filter(function(item, pos, ary) {
+            return !pos || item.map_name != ary[pos - 1].map_name;
+        });
+        
+        howBig(countries,world,all_country_coordinate,regions);
 })
 
-
-// second_countries.features = second_countries.features.filter(function(d){ return d.properties.name=="France"})
-
-
-// // console.log(second_countries.features)
-// svg.append("g")
-//     .attr("id", "movable_svg_2")
-//     .selectAll(".second_country")
-//     .data(second_countries.features)
-//     .enter()
-//     .append("path")
-//     .attr("class", "second_country")
-//       .attr("fill", "blue")
-//       .attr("d", path2)
-//       .style("stroke", "none")
-//       .call(drag); 
-
-
-// svg.append("g")
-//     .selectAll(".region") 
-//     .data(states.features)
-//     .enter()
-//     .append("path")
-//     .attr("class", "region")
-//     .attr("fill", "red")
-//     .attr("d", path)
-//     .style("stroke", "green")
-//     .on("mouseover", function(event, d) {
-//         // console.log(event)
-//         d3.select(this).style("stroke", "blue")
-//                 .attr("fill", "blue")
-        
-//     })

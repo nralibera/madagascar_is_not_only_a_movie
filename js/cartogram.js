9721bc06cf786c11f339fbb7a3d23ebf66d2e797
Nantomiaro RALIBERA <94388805+nralibera@users.noreply.github.com>
2024-02-02 16:41:23 +0100

    Add chart for global population evolution

--- js/cartogram.js
+++ js/cartogram.js
@@ -7,7 +7,7 @@ import {width, height, path, projection, drawRegion } from './app.js';
 let scale;
 
 export function drawCartogram(countries,regionsTopo,populationData){
-    // console.log(regionsTopo)
+    const stackedPopulationData = buildStackedData(populationData);
         // Draw the map
     const svg = d3.select(".myDataviz");
     const regions = topojson.feature(regionsTopo, regionsTopo.objects.states);
@@ -89,7 +89,7 @@ export function drawCartogram(countries,regionsTopo,populationData){
     .attr('fill', 'black'); 
 
 
-    // add year select and update cartogram
+    // add year select and updateMap cartogram
     const yearList = [...Array(10)].map((_,i)=>2009+i);
 
 
@@ -104,7 +104,7 @@ export function drawCartogram(countries,regionsTopo,populationData){
         .attr("list", "tickmarks")
         .attr("class", "yearSelect")
         .on("input", function() {
-            update(regions, populationData, colorScale, scaleLinear);
+            updateMap(regions, populationData, colorScale, scaleLinear);
         });
     
     const tickmarks = slider.append("datalist").attr("id", "tickmarks");
@@ -114,30 +114,44 @@ export function drawCartogram(countries,regionsTopo,populationData){
         .attr("value", d=>d)
         .text(d=>d);
 
-    update(regions,populationData,colorScale,scaleLinear);
-}
-  
-function init(cartogram,regions,scaleColor,scaleLinear){
+    updateMap(regions,populationData,colorScale,scaleLinear);
+    // updateBarChart("a",regions,stackedPopulationData);
+    
+    const divForBarChart = d3.select(".map")
+                            .append("div")
+                            .attr("class","popUpBarChart")
+                            .style("width", "100%")
+                            .style("height", "100%")
+                            .style("position","absolute")
+                            .style("top",0)
+                            .style("left",0)
+                            .style("z-index",1000)
+                            .style("background-color","rgb(2, 0, 22)")
+                            .style("display","none");
 
-    const features = cartogram.features(regions, regions.objects.states.geometries);
-    // console.log(features)
-    const regionGroup = d3.select(".all_regions");
-    regionGroup.selectAll(".regions")
-    .data(features)
-          .enter()
-          .append("path")
-            .attr("class", "regions")
-            .attr("fill", d=>scaleColor(d.properties.NAME_2))
-            .attr("d", path);
+    // Add button to show bar chart
+    d3.select(".mapOptions").append("button")
+    .text("Global population")
+    .on("click", function(){
+        d3.select(".popUpBarChart").style("display","flex");
+        updateBarChart(regions,stackedPopulationData);
+    });
+    // Add button to close bar chart
+    d3.select('.popUpBarChart').append("button")
+    .text("Close Bar Chart")
+    .on("click", function(){
+        d3.select(".popUpBarChart").style("display","none");
+    });
+
 }
 
-function update(regions,populationData,colorScale,scaleLinear){
+function updateMap(regions,populationData,colorScale,scaleLinear){
     const year = d3.select(".yearSelect").property("value");
 
     // // get all pop of 2018 from populationData in the field P_2018est as a list
     const yearString = year == 2009 ? "P_"+year :"P_"+year+"est";
 
-    // update the data
+    // updateMap the data
     d3.select(".all_regions")
         .selectAll(".regions")
         .data(regions.features, function(d){return d.properties.ID_2})
@@ -173,6 +187,139 @@ function showTooltip(event, populationData,yearString){
     const popString = pop.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
 
     tooltip.text(event.properties.NAME_2+" : "+popString)
+            .style("left", (d3.event.pageX + 20) + "px")
+            .style("top", (d3.event.pageY + 20) + "px");
+}
+
+function buildStackedData(populationData){
+    const data = [];
+    const yearList = [...Array(10)].map((_,i)=>2009+i);
+    const regions = populationData.data.map(d=>d.ADM1_EN);
+    for (let i = 0; i < yearList.length; i++) {
+        const yearString = yearList[i] == 2009 ? "P_"+yearList[i] :"P_"+yearList[i]+"est";
+        const popAtYearGroupByRegion = {};
+        popAtYearGroupByRegion.group = yearList[i];
+        regions.forEach(region => {
+            popAtYearGroupByRegion[region] = +populationData.data.filter(e=>e.ADM1_EN==region)[0][yearString];
+        });
+        data.push(popAtYearGroupByRegion);
+    }
+    return data;
+}
+
+function updateBarChart(regions,populationData){
+
+    // / set the dimensions and margins of the graph
+    const margin = {top: 10, right: 50, bottom: 20, left: 50},
+    width = 900 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+    // If svg exist use it else create it
+    let svg = d3.select(".popUpBarChart").select(".barChart g");
+
+    if (svg.empty()){
+        svg = d3.select(".popUpBarChart")
+                .append("svg")
+                .attr("class","barChart")            
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform",
+                    "translate(" + margin.left + "," + margin.top + ")");
+    }
+
+    svg.selectAll(".axis, .bar").remove(); // remove the axis if it exist
+    // console.log(d3.select(".barChart"));
+
+
+    // List of subgroups = header of the csv files = soil condition here
+    const subgroups = regions.features.map(d=>d.properties.NAME_2);
+
+    // List of groups = species here = value of the first column called group -> I show them on the X axis
+    const groups = [...Array(10)].map((_,i)=>2009+i);
+
+    // Add X axis
+    const x = d3.scaleBand()
+        .domain(groups)
+        .range([0, width])
+        .padding([0.2]);
+
+    svg.append("g")
+    .attr("class","axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x).tickSizeOuter(0));
+
+    const sumOfPopIn2018 = Object.values(populationData[populationData.length-1]).reduce((a, b) => a + b, 0) - 2018;
+    
+    // Add Y axis
+    const y = d3.scaleLinear()
+                .domain([0, sumOfPopIn2018])
+                .range([ height, 0 ]);
+
+    
+    svg.append("g")
+        .attr("class","axis")
+        .call(d3.axisLeft(y).tickFormat(d3.format(".2s")));
+
+    // color palette = one color per subgroup
+    const randomColorList = [...Array(22)].map(() => '#'+(Math.random()*0xFFFFFF<<0).toString(16).padStart(6, '0'));
+    const color = d3.scaleOrdinal()
+                    .domain(subgroups)
+                    .range(randomColorList)
+
+    //stack the data? --> stack per subgroup
+    const stackedData = d3.stack()
+                    .keys(subgroups)
+                    (populationData)
+
+                    
+    // Show the bars
+    svg.append("g")
+        .selectAll("g")
+        // Enter in the stack data = loop key per key = group per group
+        .data(stackedData, function(d) { return d.key; })
+        .join("g")
+            .attr("fill", function(d) { return color(d.key); })
+            .selectAll("rect")
+            // enter a second time = loop subgroup per subgroup to add all rectangles
+            .data(function(d) { return d; }, function(d) { return d.data.group + ':' + (d[1]-d[0]); })
+            .join("rect")
+            .attr("x", function(d) { return x(d.data.group); })
+            .attr("y", function(d) { return y(0); }) // Start at the bottom
+            .attr("height", 0) // Start with a height of 0
+            .attr("width",x.bandwidth())
+            .attr("class","bar")
+            .on("mouseover", function(event, d) {
+                showTooltipBarChart(event,d);
+            })
+            .on("mouseout", function(event, d) {
+                d3.select(".tooltip").remove();
+            });
+            
+            
+            
+        d3.selectAll(".bar")
+            .transition()
+            .duration(800)
+            .attr("y", function(d) { return y(d[1]); })
+            .attr("height", function(d) { return y(d[0]) - y(d[1]); })
+
+}
+
+function showTooltipBarChart(event){
+    
+    const tooltip = d3.select(".popUpBarChart")
+                        .append("div")
+                        .attr("class","tooltip");
+
+    const pop = event[1]- event[0];
+
+    // add space between number
+    const popString = pop.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+    // find the key where the value is equal to the pop
+    const regionName = Object.keys(event.data).find(key => event.data[key] === pop);
+    
+    tooltip.text(regionName + " : " + popString)
             .style("left", (d3.event.pageX + 20) + "px")
             .style("top", (d3.event.pageY + 20) + "px");
 }
